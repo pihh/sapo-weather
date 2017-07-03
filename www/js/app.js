36a82482e76b11b4eaef1afc97e8660254de1d42
Filipe mota de sá <pihh.rocks@gmail.com>
2017-07-03 11:25:19 +0200

    added custom objects to app.js

--- www/js/app.js
+++ www/js/app.js
@@ -1,5 +1,68 @@
 /**
- * @desc: Application base files. 
+ * @desc: Custom functionality that might be usefull in the future
+ * @author: Filipe Mota de Sá - filipemotasa@hotnail.com
+ * @todo:
+ */
+
+// Converts numbers to rads.
+ Number.prototype.toRad = function(){
+    return this * Math.PI/180;
+ };
+
+// Coordinates object
+// Used and commented a snippet I have
+var Coordinate = function(lat, lng){
+
+    //Private vars
+    var _coordinates = {},
+        _initialized = false;
+
+    // vvalidates if is a number
+    var _isNumber = function(n){
+      if(!isNaN(parseFloat(n)) && isFinite(n))
+        return true;
+      // throw an exception warning that this is not a coordinate
+      throw n + ' is not a number exception. Caller: Coordinate._isNumber(). File: app.js';
+    };
+
+    // setter
+    this.set = function(lat,lng){
+      // initialize the object once
+      if(_initialized){
+        throw 'Coordinate already initialized';
+      }
+      // var _privateMethod = function(){};
+      // this.publicMethod = function(){};
+      if(_isNumber(lat) && _isNumber(lng)){
+        _coordinates.lat = lat;
+        _coordinates.lng = lng;
+        _initialized = true;
+      }
+    };
+
+    // getter
+    this.get = function(){
+      if(_coordinates)
+        return _coordinates;
+
+      // throw an exception warning that this is not a coordinate
+      throw n + ' is not a number exception. Caller: Coordinate._isNumber(). File: app.js';
+    };
+
+    // construct if variables
+    if(lat && lng){
+      this.set(lat,lng);
+    };
+
+    // constructor
+    return this;
+};
+
+var coord = new Coordinate(10 , 10);
+console.log(typeof coord, coord instanceof Coordinate, coord.get());
+
+/**
+ * @desc: Application base files.
  * @author: Filipe Mota de Sá - filipemotasa@hotnail.com
  * @todo:
  */
--- www/js/filters/haversine.js
+++ www/js/filters/haversine.js
@@ -6,6 +6,16 @@
 
 filters.filter('haversine', function() {
 
+  var haversine = function(start,end){
+
+    // error handleing
+    // start && end should be a coordinates object
+    var lat1,lat2,lon1,lon2,radius,x1,dLat,x2,dLon,hF1,hF2,hF;
+
+    lat1 = start.lat;
+    lat2 = end.lat;
+
+  }
   // In the return function, we must pass in a single parameter which will be the data we will work on.
   // We have the ability to support multiple other parameters that can be passed into the filter optionally
   return function(input, optional1, optional2) {
