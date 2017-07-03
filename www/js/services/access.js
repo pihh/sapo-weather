6b7b02ca4b12a4af59a5600c7047320bc717442a
Filipe mota de sá <pihh.rocks@gmail.com>
2017-07-03 19:27:31 +0200

    added location service @ access -> for access level files

--- www/js/services/access.js
+++ www/js/services/access.js
@@ -4,14 +4,55 @@
  * @todo:
  */
 
-services.service('Access',function($q, STATUS) {
+services.factory('Access',function($q, STATUS, $cordovaGeolocation, $ionicLoading, $log) {
+
 
   var access = {
     "$hasLocationEnabled": function() {
 
     },
     "$getCoordinates": function() {
+      $ionicLoading.show({
+        'template': '<ion-spinner icon="bubbles"></ion-spinner><br/>Acquiring location!'
+      });
 
+      var posOptions = {
+        'enableHighAccuracy': true,
+        'timeout': 20000,
+        'maximumAge': 0
+      };
+
+      $cordovaGeolocation.getCurrentPosition(posOptions).then(function(position) {
+
+        $log.info({
+          'position': position
+        });
+
+        var lat  = position.coords.latitude;
+        var long = position.coords.longitude;
+
+        var myLatlng = new google.maps.LatLng(lat, long);
+
+        var mapOptions = {
+          'center': myLatlng,
+          'zoom': 16,
+          'mapTypeId': google.maps.MapTypeId.ROADMAP
+        };
+
+        // var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+        return {
+          'status': STATUS.OK,
+          'data': position
+        };
+      }, function(err) {
+        $log.error(err);
+        return {
+          'status': STATUS.UNAUTHORIZED,
+          'data': err
+        };
+      }).finally(function() {
+        $ionicLoading.hide();
+      });
     }
   };
 
